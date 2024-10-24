ac66b252209d28171384f702aca12d5986a73f64
Marta <marta.gilabert.guzman@gmail.com>
2024-10-24 11:43:42 +0200

    add first test

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <p>heading</p>
         <ol>
           <li>
             Get started by editing <code>app/page.tsx</code>.
