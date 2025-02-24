1fbc8abc0322330396e2f744de7cd7d8af6d55f8
Marta <marta.gilabert.guzman@gmail.com>
2025-02-24 11:29:36 +0100

    change on package.json

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Carousel from "./components/carousel";
 export default function Home() {
   return (
     <div className={styles.carrouselPage}>
+      <h1>Rick & Morty</h1>
       <Carousel />
     </div>
   );
